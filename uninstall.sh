14c7cf7f488d5c79ec97d75d1fca45f763bd24ff
MJ Stahl <hello@mjstahl.com>
2024-11-16 15:14:02 -0500

    Remove alias added to .zshrc

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Uninstalling SBCL"
+sed '/Area51/,+1 d' $HOME/.zshrc > .zshrc.tmp && mv .zshrc.tmp $HOME/.zshrc
 brew uninstall sbcl
 brew uninstall readline
 rm ~/.sbclrc
